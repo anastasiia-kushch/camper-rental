1af1e528caa8288c2d2330181d8b39ad9d69520a
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-07-20 20:01:44 +0200

    complete homepage, notfoundpage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,3 +1,10 @@
+import NotFound from '../../components/NotFound/NotFound';
+import css from './NotFoundPage.module.css';
+
 export default function NotFoundPage() {
-  return <div></div>;
+  return (
+    <div className={css.container}>
+      <NotFound />
+    </div>
+  );
 }
